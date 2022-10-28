5599ac2a5da9a40f14936964b5504d16e86e619c
yongzhengooi <yongzheng09@hotmail.com>
2022-10-29 03:10:23 +0800

    alert update

--- lib/Alert.py
+++ lib/Alert.py
@@ -4,6 +4,7 @@ import smtplib
 import os
 import platform
 from lib.Logging import *
+# from Logging import Logging
 from email.message import EmailMessage
 from dotenv import load_dotenv
 _=load_dotenv("var.env")
@@ -16,26 +17,39 @@ class Alert:
         self.msg_title=msg_title
         self.msg_content=msg_content
         self.imgIcon="resources\\logo.ico"
+        self.emailList=self.getEmailList()
 
-    def generateDesktopNotification(self):
-        win10toast.ToastNotifier().show_toast(self.msg_title,self.msg_content,self.imgIcon,5)
+    def getEmailList(self):
+        emailArray=[]
+        with open("miscellaneous\\email.txt","r") as file:
+            line = file.readlines()
+            for item in line:
+                emailArray.append(item.replace("\n",""))
+            file.close()
+            return emailArray
 
-    def sendEmail(self):
+    def generateDesktopNotification(self,thread=False):
+        win10toast.ToastNotifier().show_toast(self.msg_title,self.msg_content,self.imgIcon,5,threaded=thread)
+
+    def sendEmail(self,personalEmail=None):
         try:
             address = os.environ.get("EMAIL_ADDRESS")
             passw = os.environ.get("EMAIL_PASSWORD")
-            emailArray=["febob54091@bongcs.com"]
             msg = EmailMessage()
             msg['Subject'] = self.msg_title
             msg['From'] = address
-            msg['To'] = emailArray
+            if not personalEmail == None:
+                 mail=[personalEmail]
+                 msg['To'] = mail
+            else:
+                msg['To'] = self.emailList
             msg.set_content(self.msg_content)
 
             with smtplib.SMTP_SSL('smtp.gmail.com', port=465) as smtp:
                 smtp.login(address, passw)
                 smtp.send_message(msg)
                 smtp.quit()
-            Logging.logInfo("Successful send the email to {}".format(str(emailArray)))
+            Logging.logInfo("Successful send the email to {}".format(str(self.emailList)))
         except Exception as e:
             Logging.logException(str(e))
         
