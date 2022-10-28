45f41549ae43a67ae5b11e7b26dca15a3841252f
yongzhengooi <yongzheng09@hotmail.com>
2022-10-29 03:10:56 +0800

    add export function

--- lib/Export.py
+++ lib/Export.py
@@ -1,2 +1,29 @@
+import pandas as pd
+from datetime import datetime
 class Export:
-    pass
+    def __init__(self,csvFile,path,name=None):
+        self.data=pd.read_csv(csvFile)
+        self.name=str(csvFile).replace(".csv","").split("\\")[-1]
+        if path =="":
+            self.path="exported"
+        else:
+            self.path=path
+        self.df=pd.DataFrame(self.data)
+    
+    def to_XML(self):
+        self.df.to_xml(path_or_buffer="{}\\{}.xml".format(self.path,self.name))
+
+    def to_JSON(self):
+        self.df.to_json(path_or_buf="{}\\{}.json".format(self.path,self.name))
+
+    def to_HTML(self):
+        self.df.to_html(buf="{}\\{}.html".format(self.path,self.name))
+
+    def to_parquet(self):
+        self.df.to_parquet(path="{}\\{}.parquet".format(self.path,self.name))
+
+    def to_excel(self):
+        self.df.to_csv(path_or_buf="{}\\{}.xlsx".format(self.path,self.name))
+
+    def to_pickle(self):
+        self.df.to_pickle(path="{}\\{}.pickle".format(self.path,self.name))
